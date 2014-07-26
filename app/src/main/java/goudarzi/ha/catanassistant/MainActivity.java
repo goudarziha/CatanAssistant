141b616d8d2b9cb0a4e28b594d82f0177d349f15
goudarziha <goudarziha@gmail.com>
2014-07-26 15:34:07 -0400

    changed font color, button transparency and moved strings to strings.xml

--- app/src/main/java/goudarzi/ha/catanassistant/MainActivity.java
+++ app/src/main/java/goudarzi/ha/catanassistant/MainActivity.java
@@ -5,16 +5,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     Button start, timer, about;
     Boolean timeChange = false;
+    TextView tv1, tv2;
     String newTime;
-    TextView time, tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +42,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (view.getId()) {
             case R.id.bStart:
                 Intent intent = new Intent(this, Start.class);
+                if (!timeChange) {
+                    newTime = "2";
+                }
+                intent.putExtra("time", newTime);
                 startActivity(intent);
                 break;
             case R.id.bTimer:
-
+                setTimer();
                 break;
             case R.id.bAbout:
                 about();
@@ -50,16 +57,39 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    public void setTimer() {
+        AlertDialog.Builder lb = new AlertDialog.Builder(this);
+        lb.setTitle("Set Timer");
+        lb.setMessage("Input minutes");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        lb.setView(input);
+        lb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                newTime = input.getText().toString();
+                Toast.makeText(getApplicationContext(), "Timer changed.", Toast.LENGTH_SHORT).show();
+                timeChange = true;
+            }
+        });
+        lb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                return;
+            }
+        });
+        lb.create().show();
+    }
+
     public void about() {
         AlertDialog.Builder lb = new AlertDialog.Builder(this);
         lb.setTitle("About");
-        lb.setMessage("Catan Assistant was created to assist players who require dice and a timer to make sure " +
-                "the game stays fluid and fun. You have the option to set the countdown time, as well as " +
-                "checking the robber box, if the robber is in play. - Amir")
+        lb.setMessage(R.string.aboutMsg)
                 .setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
+                timeChange = false;
             }
         });
         lb.create().show();
